882f7227c5fa21ddffa632df02687b318563032a
Kulonbekov <kulonbekov@gmail.com>
2021-03-21 21:33:18 +0600

    added Home Work 18.1 and class Cat,Eat,..

---
+++ src/main/kotlin/HW_18.1.kt
@@ -0,0 +1,43 @@
+fun main(){
+    val cat = Cat("Kotik", 4)
+    val eat = Eat("Plov", "Vtoroe blyudo")
+    val paper = Paper()
+    val boiler = Boiler()
+    val battery = Battery()
+    val liquid = Liquid()
+    val cup = Cup()
+    val box = Box()
+    val student = Student()
+    val companyMember = CompanyMember()
+
+}
+class Cat(val name: String, val age: Int){
+
+}
+class Eat(val name:String, val kindOffood: String){
+
+}
+class Paper(){
+
+}
+class Boiler(){
+
+}
+class Battery(){
+
+}
+class Liquid(){
+
+}
+class Cup(){
+
+}
+class Box(){
+
+}
+class Student(){
+
+}
+class CompanyMember(){
+
+}
